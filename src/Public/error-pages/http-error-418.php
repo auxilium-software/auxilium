edcdfdacb09911de1caa0f0746d7b5e454091bd6
Cerys <cerysamberlewis@gmail.com>
2024-12-02 00:34:43 +0000

    "

--- src/Public/error-pages/http-error-418.php
+++ src/Public/error-pages/http-error-418.php
@@ -2,7 +2,9 @@
 
 use Auxilium\TwigHandling\PageBuilder;
 
-require_once "../Environment.php";
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../Configuration/Configuration/Environment.php';
+
 $pb = PageBuilder::get_instance();
 $pb->setVariable("error_code", 418);
 $pb->setTemplate("ErrorPages/HTTPError");
