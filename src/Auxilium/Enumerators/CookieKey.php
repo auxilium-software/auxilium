a99262aece9d67c228337b0056ca91081bf7a6fe
Cerys <cerysamberlewis@gmail.com>
2024-12-05 21:31:19 +0000

    typo fix

--- src/Auxilium/Enumerators/CookieKey.php
+++ src/Auxilium/Enumerators/CookieKey.php
@@ -5,7 +5,7 @@ namespace Auxilium\Enumerators;
 enum CookieKey: string
 {
     case LANGUAGE = "lang";
-    case PROGRESSIVE_LOAD = "progressive_load";
+    case PROGRESSIVE_LOAD = "progressiveload";
     case SESSION_KEY = "session_key";
     case STYLE = "style";
 }
