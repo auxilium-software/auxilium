b955b545bd42746bbad20d445a6b78b14c9d49ce
Cerys <cerysamberlewis@gmail.com>
2025-01-21 11:35:34 +0000

    removed redundant code

--- src/Public/onboard-new-case.php
+++ src/Public/onboard-new-case.php
@@ -6,4 +6,3 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../Configuration/Configuration/Environment.php';
 
 NavigationUtilities::Redirect(target: "/form/8c5b68fb-b79d-4b5f-b3bb-984ee3214891");
-exit();
