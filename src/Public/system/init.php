f13cd83b2de8aa9203e046a90d0321b2f1c4b1bb
Cerys <cerysamberlewis@gmail.com>
2025-03-03 17:36:35 +0000

    beneficiaries can now see their case workers

--- src/Public/system/init.php
+++ src/Public/system/init.php
@@ -112,6 +112,7 @@ else
         // case permissions
         "GRANT READ,WRITE ON /cases/# ON /cases/#/*",   // grant read and write permissions on the user's cases
         "GRANT READ,WRITE ON /cases/#/todos/#",         // grant read and write (for deletion) permissions on the to do items on a case
+        "GRANT READ ON /cases/#/workers/#/*",           // grant read permissions for case beneficiaries to see their caseworkers
         // misc permissions
         "GRANT READ ON /messages/#",                    //
         "GRANT READ,WRITE,DELETE ON /assigned_cases/# ON /assigned_cases/#/* ON /assigned_cases/#/messages/# DELEGATABLE",  //
