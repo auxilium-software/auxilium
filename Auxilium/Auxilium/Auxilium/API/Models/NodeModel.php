33459a5062e1b3ff427baf1adfdd6cd1186f6524
Cerys <cerysamberlewis@gmail.com>
2025-04-22 13:19:18 +0100

    started on docs for the previous commit

--- Auxilium/Auxilium/Auxilium/API/Models/NodeModel.php
+++ Auxilium/Auxilium/Auxilium/API/Models/NodeModel.php
@@ -3,11 +3,30 @@
 namespace Auxilium\Auxilium\API\Models;
 
 use Auxilium\Auxilium\API\Enumerators\APIResponseStatus;
+use OpenApi\Attributes\Property;
 
 class NodeModel
 {
+    #[Property(
+        property: "Status",
+        description: "The status of the API request.",
+        type: "string",
+        nullable: false,
+    )]
     public APIResponseStatus $Status;
+    #[Property(
+        property: "ResponseCode",
+        description: "The HTTP Status code.",
+        type: "int",
+        nullable: false,
+    )]
     public int $ResponseCode;
+    #[Property(
+        property: "ErrorText",
+        description: "If an error occurs, the message will be placed here.",
+        type: "string",
+        nullable: false,
+    )]
     public ?string $ErrorText = null;
 
 
