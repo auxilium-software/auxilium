1176794537b64104bb2a472bcd2451337a8b5224
Cerys <cerysamberlewis@gmail.com>
2025-05-13 22:30:20 +0100

    Update PrimaryRouter.php

--- Auxilium/Routing/PrimaryRouter.php
+++ Auxilium/Routing/PrimaryRouter.php
@@ -76,12 +76,38 @@ if(file_exists($file))
 $file = $publicDir . $path;
 if(file_exists($file))
 {
+    $extension = pathinfo($file, PATHINFO_EXTENSION);
+    $mimeTypes = [
+        'css'  => 'text/css',
+        'js'   => 'application/javascript',
+        'json' => 'application/json',
+        'png'  => 'image/png',
+        'jpg'  => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'gif'  => 'image/gif',
+        'svg'  => 'image/svg+xml',
+        'woff' => 'font/woff',
+        'woff2'=> 'font/woff2',
+        'ttf'  => 'font/ttf',
+        'eot'  => 'application/vnd.ms-fontobject',
+        'otf'  => 'font/otf',
+        'html' => 'text/html',
+        'txt'  => 'text/plain',
+        'pdf'  => 'application/pdf',
+        'mp4'  => 'video/mp4',
+    ];
     $finfo = finfo_open(FILEINFO_MIME_TYPE);
-    $mimeType = finfo_file($finfo, $file);
+    $mimeType = finfo_file($finfo, $file) ?: 'application/octet-stream';
     finfo_close($finfo);
+
+    if (isset($mimeTypes[$extension]))
+    {
+        $mimeType = $mimeTypes[$extension];
+    }
 
     header("Content-Type: $mimeType");
     header("Content-Length: " . filesize($file));
+
     return false;
 }
 
