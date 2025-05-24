3b5da406544ecf1e0f09d17df36696ab18ce627c
Cerys <cerysamberlewis@gmail.com>
2025-05-24 18:18:39 +0100

    redis credential management

--- Auxilium/Auxilium/DatabaseInteractions/Redis/RedisServerConnection.php
+++ Auxilium/Auxilium/DatabaseInteractions/Redis/RedisServerConnection.php
@@ -14,6 +14,11 @@ class RedisServerConnection
     public function __construct()
     {
         $this->RedisClient = new Client([
+            'scheme'    => 'tcp',
+            'host'      => INSTANCE_CREDENTIAL_REDIS_HOST,
+            'port'      => INSTANCE_CREDENTIAL_REDIS_PORT,
+            'password'  => INSTANCE_CREDENTIAL_REDIS_PASSWORD,
+            'database'  => INSTANCE_CREDENTIAL_REDIS_DATABASE,
         ]);
     }
 
