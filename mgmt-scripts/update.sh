9a3511f812fe11c5ab5142b3e04c471f5963b22e
Cerys <cerysamberlewis@gmail.com>
2026-05-25 15:43:49 +0100

    Update update.sh

--- mgmt-scripts/update.sh
+++ mgmt-scripts/update.sh
@@ -6,17 +6,20 @@ export DOTNET_CLI_TELEMETRY_OPTOUT=1
 dotnet tool install --global dotnet-ef
 
 
-cd ${pwd}/auxilium-services--admin-tools/AuxiliumSoftware.AuxiliumServices.AdministrationTools
+cd auxilium-services--admin-tools/AuxiliumSoftware.AuxiliumServices.AdministrationTools
 dotnet clean
 dotnet build
+cd ../../
 
-cd ${pwd}/auxilium-services--api/AuxiliumSoftware.AuxiliumServices.API
+cd auxilium-services--api/AuxiliumSoftware.AuxiliumServices.API
 dotnet clean
 dotnet build
+cd ../../
 
-cd ${pwd}/auxilium-services--task-runner/AuxiliumSoftware.AuxiliumServices.BackgroundTaskRunner
+cd auxilium-services--task-runner/AuxiliumSoftware.AuxiliumServices.BackgroundTaskRunner
 dotnet clean
 dotnet build
+cd ../../
 
 exit 0
 
