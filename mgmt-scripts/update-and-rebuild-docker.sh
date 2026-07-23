fb236a6be3170e631bc8de43615bc08c6efc9ae8
Cerys <cerysamberlewis@gmail.com>
2026-07-23 14:00:29 +0100

    Create update-and-rebuild-docker.sh

---
+++ mgmt-scripts/update-and-rebuild-docker.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -euo pipefail
+
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+
+dotnet tool install --global dotnet-ef
+
+
+
+git reset --hard HEAD
+git pull
+git submodule update --init --recursive
+
+
+cd auxilium-services--admin-tools/AuxiliumSoftware.AuxiliumServices.AdministrationTools
+dotnet clean
+dotnet build
+cd ../../
+
+cd auxilium-services--api/AuxiliumSoftware.AuxiliumServices.API
+dotnet clean
+dotnet build
+cd ../../
+
+cd auxilium-services--task-runner/AuxiliumSoftware.AuxiliumServices.BackgroundTaskRunner
+dotnet clean
+dotnet build
+cd ../../
+
+
+
+
+
+
+
+cd auxilium-portal
+sudo docker compose build
+
+cd auxilium-services--admin-tools
+sudo docker compose build
+
+cd auxilium-services--api
+sudo docker compose build
+
+cd auxilium-services--task-runner
+sudo docker compose build
+
+
+
+
+exit 0
+
